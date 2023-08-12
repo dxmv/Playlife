65d70ac75e4a257f1d6bfd6fc48fcdfc480bdced
dxmv <dimitirjestepanovic1@gmail.com>
2023-08-12 21:53:46 +0200

    Structure and tailwind setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Progress } from "./pages/progress/Progress";
+
+function App() {
+	return <Progress />;
+}
+
+export default App;
---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Navigation = () => {
+	return (
+		<nav>
+			<ul>
+				<li className="text-xl">Progress</li>
+				<li>Quests</li>
+				<li>How To Use</li>
+				<div>
+					<li>Settings</li>
+					<li>Profile</li>
+				</div>
+			</ul>
+		</nav>
+	);
+};
